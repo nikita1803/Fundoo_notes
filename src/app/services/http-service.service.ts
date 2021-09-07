878cfc1c420f8b3a2c1f07e5548c94dae3421c9f
Nikita Rai <rainikita595@gmail.com>
2021-09-07 09:12:43 +0530

    [Nikita] Modify: added a note service to cl get and post method

--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpServiceService {
-
+  token : any;
+  BaseUrl = environment.baseUrl;
   constructor(private httpClient: HttpClient) { }
 
   post(url: string, data: any, isHeaderRequired: any = false, token: any = null) {
@@ -13,5 +15,11 @@ export class HttpServiceService {
     let tokenOption = {headers: new HttpHeaders({"Authorization": token})};
     return this.httpClient.post(url, data, isHeaderRequired && tokenOption)
   }
+  
+get(url: string, isHeaderRequired: any = false, token: any = null) {  
+  console.log("data Get");
+  let tokenOption = {headers: new HttpHeaders({"Authorization": token})};
+  return this.httpClient.get(url, isHeaderRequired && tokenOption)
+}
 
 }
---
+++ src/app/services/noteservice.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpServiceService } from './http-service.service';
+import { environment } from 'src/environments/environment';
+import {tap} from 'rxjs/operators';
+import { Subject } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteserviceService {
+  
+  private refresh = new Subject<void>();
+  constructor(private httpService: HttpServiceService) { }
+
+  url = environment.baseUrl
+
+  createnote = (userData: any, token: any) => {
+    return this.httpService.post(`${this.url}notes/addNotes`, userData, true, token).pipe(
+      tap(() => {
+        this.refresh.next();
+      })
+    );
+  }
+
+  note = (token: any) => {
+    return this.httpService.get(`${this.url}notes/getNotesList`, true, token)
+  }
+ 
+}
