4377f339995f0efb8a21518303443c300cf08e8a
Nikita Rai <rainikita595@gmail.com>
2021-09-07 09:09:58 +0530

    [Nikita] Modify: api cl for data storage in localhost

--- src/app/components/createnote/createnote.component.ts
+++ src/app/components/createnote/createnote.component.ts
@@ -1,39 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { NoteserviceService } from 'src/app/services/noteservice.service';
-import { FormGroup, FormControl  } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { EventEmitter } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-createnote',
   templateUrl: './createnote.component.html',
   styleUrls: ['./createnote.component.scss']
 })
 export class CreatenoteComponent implements OnInit {
-
-  constructor(private service: NoteserviceService ) { }
-  step = 0;
-  nextStep() {
-    this.step++
-  }
-  ngOnInit(): void {
-  }
+  
+  constructor(private service: NoteserviceService , private activeRoute: ActivatedRoute) { }
   form = new FormGroup({
+    titleText: new FormControl(''),
     notesText: new FormControl('')
   })
-  getf() { return this.form.controls}
   tokenId = localStorage.getItem("token");
-
+  ngOnInit(): void {
+  }
   submit=() => {
     let userData = {
-      
-      "description": this.form.controls.notestext.value,
-      
+      "title": this.form.controls.titleText.value,
+      "description": this.form.controls.notesText.value,
+      "tokenid" : this.tokenId
     } 
     this.service.createnote(userData, this.tokenId).subscribe((userData) => {
       console.log(userData)
-      this.form.reset();
+      this.form.reset(); 
+      
     });
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.form.value))
-
     
   }
-
+ 
 }
