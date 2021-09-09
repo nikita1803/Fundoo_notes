977b544e1162e7464b4fb21347d3b8ba77a2b92f
Nikita Rai <rainikita595@gmail.com>
2021-09-09 12:23:51 +0530

    [Nikita] Modify: color pallet is added to color icon

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Output, EventEmitter, Input} from '@angular/core';
+import { NoteserviceService } from 'src/app/services/noteservice.service';
 
 @Component({
   selector: 'app-icons',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icons.component.scss']
 })
 export class IconsComponent implements OnInit {
+  @Output() transColor:EventEmitter<string> = new EventEmitter<string>();
+  @Input() noteId:any;
+
+  isColor:string=''
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  receiveColorCode = ($isColor:string) =>{
+    console.log("icon " + $isColor);
+    this.isColor = $isColor;
+    console.log("singleIcon " + this.isColor)
+    this.transColor.emit(this.isColor)
   }
 
 }
