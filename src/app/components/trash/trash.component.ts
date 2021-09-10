bc058d96e02bf64263119f8c49302bc490bc93d7
Nikita Rai <rainikita595@gmail.com>
2021-09-10 22:51:06 +0530

    [Nikita] Add: deleted file is stored in trash folder

---
+++ src/app/components/trash/trash.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { NoteserviceService } from 'src/app/services/noteservice.service';
+
+@Component({
+  selector: 'app-trash',
+  templateUrl: './trash.component.html',
+  styleUrls: ['./trash.component.scss']
+})
+export class TrashComponent implements OnInit {
+  @Input() notes: any = [];
+  trashNote:any=[];
+  
+  
+  token_Id = localStorage.getItem('token');
+
+  constructor(private noteService: NoteserviceService) { }
+
+  ngOnInit(): void {
+    this.getAllTrash();
+    this.noteService.getRefreshedData().subscribe(() => this.getAllTrash());
+  }
+  getAllTrash(){
+    console.log("Getting trash")
+    this.trashNote=this.noteService.getTrashNotes( this.token_Id).subscribe((data:any)=>{
+      this.notes=data['data'].data 
+    })
+  }
+
+}
