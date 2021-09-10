33d6c8911f8b53a67fcdd83d744181218f10611d
Nikita Rai <rainikita595@gmail.com>
2021-09-10 22:49:01 +0530

    [Nikita] Modify: delete api is done

--- src/app/components/displaynote/displaynote.component.ts
+++ src/app/components/displaynote/displaynote.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NoteserviceService } from 'src/app/services/noteservice.service';
+import { UpdateComponent } from '../update/update.component';
 @Component({
   selector: 'app-displaynote',
   templateUrl: './displaynote.component.html',
@@ -9,17 +10,28 @@ import { NoteserviceService } from 'src/app/services/noteservice.service';
 
 export class DisplaynoteComponent implements OnInit {
   @Input() allNotes: any = [];
-  //noteGen: any;
+  noteGen: any = [];
+  updateData: any;
   tokenId = localStorage.getItem("token");
   
-  constructor(public note: NoteserviceService, private mate: MatDialog) { }
+  constructor(public note: NoteserviceService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
     
+    
     this.note.note( this.tokenId).subscribe((userData:any) => {
+
       this.allNotes=userData['data'].data
-       
-     });
+      this.allNotes=userData['data'].data.reverse()
+      this.allNotes=this.allNotes.filter((noteData:any)=>{
+       return noteData.isDeleted === false ;
+      });
+    })
   }
+  openAddDialog(updateData: any) {
+   this.updateData = updateData
+    this.dialog.open(UpdateComponent, {data : {note: updateData} });
+  }
+
   
 }
