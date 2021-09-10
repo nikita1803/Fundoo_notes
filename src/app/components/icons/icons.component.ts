a8da951395640b5ee2c672f615feba2cdf6861ac
Nikita Rai <rainikita595@gmail.com>
2021-09-10 22:56:24 +0530

    [Nikita] modify: delete function is call in this

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit ,Output, EventEmitter, Input} from '@angular/core';
 import { NoteserviceService } from 'src/app/services/noteservice.service';
-
+import { CreatenoteComponent } from '../createnote/createnote.component';
+import { DisplaynoteComponent } from '../displaynote/displaynote.component';
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
   styleUrls: ['./icons.component.scss']
 })
 export class IconsComponent implements OnInit {
-  @Output() transColor:EventEmitter<string> = new EventEmitter<string>();
   @Input() noteId:any;
 
-  isColor:string=''
-
-  constructor() { }
+  token_Id = localStorage.getItem('token');
+  constructor(public note: NoteserviceService) { }
 
   ngOnInit(): void {
   }
-  receiveColorCode = ($isColor:string) =>{
-    console.log("icon " + $isColor);
-    this.isColor = $isColor;
-    console.log("singleIcon " + this.isColor)
-    this.transColor.emit(this.isColor)
+  
+  delete(){
+    console.log(this.noteId);
+    //alert(this.token_Id)
+    let data = {
+      noteIdList:[this.noteId],
+      isDeleted:true
+    }
+    this.note.deleteNotes(data, this.token_Id).subscribe((data)=>{
+      console.log("Deleted Successfully", data);
+    });
   }
-
 }
