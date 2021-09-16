21a52767f89aa15c94cc4fb252dde33ed19bd74b
nikita1803 <rainikita595@gmail.com>
2021-09-16 22:12:55 +0530

    [Nikita] Modify: Title color stored in the array

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -10,9 +10,9 @@ import { EmailValidator, FormBuilder, FormControl, FormGroup, Validators } from 
 export class ResetPasswordComponent implements OnInit {
 
   registerForm: FormGroup;
-  submitted = false;
+  submitted  = false;
   constructor(private formBuilder: FormBuilder) {
-    this.registerForm = this.formBuilder.group({
+    this.registerForm  = this.formBuilder.group({
       password: ['', [Validators.required, Validators.minLength(6)]],
       password_confirm: ['', [Validators.required, Validators.minLength(6)]],
     }); }
@@ -24,7 +24,7 @@ export class ResetPasswordComponent implements OnInit {
   get f() { return this.registerForm.controls; }
 
   onSubmit() {
-    this.submitted = true;
+    this.submitted  = true;
     console.log(this.registerForm.value)
 
     if(this.registerForm.invalid) {
@@ -32,6 +32,8 @@ export class ResetPasswordComponent implements OnInit {
     }
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
   }
+  colors =['color:#3566b8','color:#34a853', 'color:#ea4335','color:#3566b8','color:#fabb05','color: #34a853','color:#ea4335','color:#34a853','color:#fabb05','color:#3566b8','color:#e6c9a8']
+  fontText =['F','u','n','d','o','o','n','o','t','e','s']
  
 }
 
