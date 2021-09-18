cc92733a25f4a4fafeedc55020daa7aa539106c3
nikita1803 <rainikita595@gmail.com>
2021-09-18 06:21:37 +0530

    [Nikita] Modify: color paltter and update pop up is added

--- src/app/components/archive/archive.component.ts
+++ src/app/components/archive/archive.component.ts
@@ -1,27 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NoteserviceService } from 'src/app/services/noteservice.service';
+import { ArchiveUpdateComponent } from '../archive-update/archive-update.component';
 @Component({
   selector: 'app-archive',
   templateUrl: './archive.component.html',
   styleUrls: ['./archive.component.scss']
 })
 export class ArchiveComponent implements OnInit {
-  archieveNote:any=[];
-  notes:any=[];
+  @Input() archieveNote: any = [];
+  notes: any = [];
+  updateData: any;
+  colorData:string=''
+  token_Id = localStorage.getItem("token");
+  
 
-  token_Id = localStorage.getItem('token');
-
-  constructor(private noteService: NoteserviceService ) { }
+  constructor(private noteService: NoteserviceService,private dialog: MatDialog ) { }
 
   ngOnInit(): void {
     this.getArchieve();
     this.noteService.getRefreshedData().subscribe(() => this.getArchieve());
   }
-
+  receiveToUpdate=($colorData:string) => {
+    this.colorData = $colorData;
+    console.log("display " + this.colorData) 
+  }
+  openAddDialog(updateData: any) {
+    this.updateData = updateData
+     this.dialog.open(ArchiveUpdateComponent, {data : {note: updateData} });
+   }
   getArchieve(){
     console.log("getting archive")
     this.archieveNote=this.noteService.getArchieveNotes(this.token_Id).subscribe((data:any)=>{
-      this.notes=data['data'].data 
+      this.notes=data['data'].data
     })
+    
+    
   }
 }
