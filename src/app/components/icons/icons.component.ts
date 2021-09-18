b4105eff1daa461373ba8dda1bfe0410b3779a26
nikita1803 <rainikita595@gmail.com>
2021-09-18 06:36:00 +0530

    [Nikita] Modify: pin part is added

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -21,8 +21,20 @@ export class IconsComponent implements OnInit {
     this.isColor = $isColor;
     console.log("singleIcon " + this.isColor)
     this.transColor.emit(this.isColor)
+    
   }
-  
+  pin(){
+    console.log(this.noteId)
+    let dataPin = {
+      noteIdList:[this.noteId],   
+      "isPined": true
+    }
+    console.log("note and boolean ", dataPin);
+    this.note.pinedNote(dataPin, this.token_Id).subscribe((dataPin)=>{
+      console.log("Pined Successfully", dataPin);
+      
+    });
+  }
   delete(){
     console.log(this.noteId);
     let data = {
@@ -31,8 +43,10 @@ export class IconsComponent implements OnInit {
     }
     this.note.deleteNotes(data, this.token_Id).subscribe((data)=>{
       console.log("Deleted Successfully", data);
-    });
+    })
+    
   }
+  
   archivePage() {
     console.log(this.noteId)
     let dataArchive = {
